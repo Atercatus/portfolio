022bcd25c9ea3e0933f58e3d9ef7d7f864bb1a5c
Atercatus <myleelee2053@naver.com>
2020-05-29 10:16:24 +0900

    :sparkles: constants 추가

---
+++ src/common/constants/index.ts
@@ -0,0 +1,22 @@
+export const SCREEN_WIDTH = {
+  MOBILE: 700,
+  WIDE_MOBILE: 1080,
+  MAIN_DESKTOP: 1440,
+};
+
+export const PAGE_INDEX: { [key: string]: string } = {
+  WEDEV: "wedev",
+  AUTONOMOUS_RC_CAR: "autonomous-rc-car",
+  AQUA_SIM: "aqua-sim",
+  A_SERIES: "a-series",
+  A_PLANET: "a-planet",
+};
+
+export const PROJECT_DETAIL_MAX_WIDTH = 96;
+
+export const LINK_ADDRESS = {
+  FACEBOOK:
+    "https://www.facebook.com/profile.php?id=100038719361512&ref=bookmarks",
+  GITHUB: "https://github.com/Atercatus",
+  BLOG: "https://contentful-atercatus-blog.myleelee2053.now.sh/",
+};
---
+++ src/common/constants/palette.ts
@@ -0,0 +1,3 @@
+export const SUBTITLE_COLOR: string[] = ["#ff8171", "#5995C8", "#ff8171"];
+
+export const LINK_COLOR: string[] = ["#ff7a71", "#7699FC"];
